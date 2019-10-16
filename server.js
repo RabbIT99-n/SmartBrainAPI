ca64b660a86171689f864b004d4af1680691451b
unknown <naida.dozic@gmail.com>
2019-10-16 20:16:04 +0200

    Changed for deployement

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
   next();
-});
+}); // CORS error -not solved
 
 
 
